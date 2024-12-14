1d0f316965954cdf167f9f9a49f2772359677a60
Md Equram Ansari <125960610+md-equram-ansari@users.noreply.github.com>
2024-12-14 19:53:37 +0530

    Add files via upload

---
+++ javabasics.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class javabasics {
+    public static void main(String args[]){
+   Scanner Sc = new Scanner (System.in);
+   boolean  input = Sc.nextBoolean();
+   System.out.println(input);
+  
+   
+       
+    }
+    
+}
